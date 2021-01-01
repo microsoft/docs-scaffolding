9850c6e08cf0605a64d0f33f3c823ac7b3b5bc2c
Jamar Warren <jamarw@users.noreply.github.com>
2020-12-31 22:33:37 -0800

    handle includes

--- src/helper/unit.ts
+++ src/helper/unit.ts
@@ -7,7 +7,7 @@ const replace = require("replace-in-file");
 export function getSelectedFile(uri: Uri, moveDown: boolean) {
     const selectedFileFullPath = parse(uri.fsPath);
     const selectedFileDir = selectedFileFullPath.dir;
-    const currentFilename = selectedFileFullPath.base;
+    const currentFilename = selectedFileFullPath.name;
     const fileNumberRegex = /(.*?)-.*/;
     const fileNumber = currentFilename.match(fileNumberRegex);
     const num: any = parseInt(fileNumber![1]);
@@ -18,7 +18,16 @@ export function getSelectedFile(uri: Uri, moveDown: boolean) {
         newUnitNumber = num - 1;
     }
     const newFilename = currentFilename.replace(num, newUnitNumber);
-    const currentFilePath = join(selectedFileDir, currentFilename);
-    const newFilePath = join(selectedFileDir, newFilename);
+    const currentFilePath = join(selectedFileDir, `${currentFilename}.yml`);
+    const newFilePath = join(selectedFileDir, `${newFilename}.yml`);
     fse.rename(currentFilePath, newFilePath);
+    const currentIncludePath = join(selectedFileDir, 'includes', `${currentFilename}.md`);
+    const newIncludePath = join(selectedFileDir, 'includes', `${newFilename}.md`);
+    fse.rename(currentIncludePath, newIncludePath);
+    const options = {
+        files: newFilePath,
+        from: currentFilename,
+        to: newFilename,
+      };
+      const results = replace.sync(options);
 }
