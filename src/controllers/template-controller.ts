bd89ca5c2e18109a8ffe83e46a02a2d456750f87
Jamar Warren <jamarw@users.noreply.github.com>
2021-03-04 18:18:04 -0800

    Improve offline support

--- src/controllers/template-controller.ts
+++ src/controllers/template-controller.ts
@@ -2,20 +2,29 @@ import { join } from "path";
 import { extensionPath } from '../extension';
 import { postError, showStatusMessage } from '../helper/common';
 import { templateRepo } from '../helper/user-settings';
+import { homedir } from 'os';
+import { copyFileSync, existsSync, mkdirSync } from "fs";
 
 export let localTemplateRepoPath: string;
 let templateZip: string;
+const fs = require("fs");
 
 export async function downloadTemplateZip() {
   const download = require('download');
   const tmp = require('tmp');
+  const docsAuthoringHomeDirectory = join(homedir(), 'Docs Authoring');
+  const offlineZip = join(docsAuthoringHomeDirectory, 'learn-scaffolding-main.zip');
+  if (!existsSync(docsAuthoringHomeDirectory)) { 
+    mkdirSync(docsAuthoringHomeDirectory);
+  }
   localTemplateRepoPath = tmp.dirSync({ unsafeCleanup: true }).name;
   showStatusMessage(`Temp working directory ${localTemplateRepoPath} has been created.`);
   try {
     await download(templateRepo, localTemplateRepoPath);
     templateZip = join(localTemplateRepoPath, 'learn-scaffolding-main.zip');
+    copyFileSync(templateZip, offlineZip);
   } catch (error) {
-    templateZip = join(extensionPath, 'offline-template-zip', 'learn-scaffolding-main.zip');
+    templateZip = offlineZip;
     postError(error);
     showStatusMessage(`Error downloading templates from ${templateRepo}. Loading local templates.`);
   }
