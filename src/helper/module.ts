8012e2811c29b11a552c21e43819035aa710e637
Jamar Warren <jamarw@users.noreply.github.com>
2021-03-17 08:46:40 -0700

    Update module.ts

--- src/helper/module.ts
+++ src/helper/module.ts
@@ -180,15 +180,8 @@ export function moduleCleanup(moduleName: string, modulePath: string) {
   // remove stub comments
   let options = {
     files: `${modulePath}/*.yml`,
-    from: /#\s?stub.*/g,
-    to: ' ',
-  };
-  replace.sync(options);
-
-  options = {
-    files: `${modulePath}/*.yml`,
-    from: /\s+$/g,
-    to: '',
+    from: /\s?#\s?stub.*/gm,
+    to: ``,
   };
   replace.sync(options);
 
